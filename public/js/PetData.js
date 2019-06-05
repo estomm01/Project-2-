ae8f7f8e54da92a3fdfe557655413de3e9048e74
Markus Maasikas <markus@hotmail.com>
2019-06-05 18:29:52 -0400

    update petData.js

--- public/js/PetData.js
+++ public/js/PetData.js
@@ -17,7 +17,6 @@ var animal = [
     Energetic: 2,
     Clean: 1,
     catorDog: 2,
-    bio: "You would prefer a cat that is very active and intelligent. Keeping a cat like this occupied and busy is an enjoyable part of your day. Variety keeps things interesting. You don't mind a cat that is a bit fearful in new situations or around new people. You can be patient and take the time to teach him that there is nothing to fear. You prefer an affectionate cat that will snuggle with you, but you don't want a cat that is always in your face trying to get attention. A cat that is intuitive about your feelings would be ideal."
   },
   {
     routeName: "dog",
@@ -34,7 +33,6 @@ var animal = [
     Energetic: 5,
     Clean: 2,
     catorDog: 1,
-    bio: "You would prefer a dog that is very active and intelligent. Keeping a dog like this occupied and busy is an enjoyable part of your day. You like a dog that is fairly easy to train to basic commands and a few fun tricks. He should be able to focus his attention long enough to learn and interact with you, but it's also OK if he decides to be distracted once in a while. Variety keeps things interesting. You don't mind a dog that is a bit fearful in new situations or around new people. You can be patient and take the time to teach him that there is nothing to fear. You prefer an affectionate dog that will snuggle with you, but you don't want a dog that is always in your face trying to get attention. A dog that is intuitive about your feelings would be ideal."
   },
   {
 
@@ -52,7 +50,6 @@ var animal = [
     Energetic: 2,
     Clean: 2,
     catorDog: 0,
-    bio: "You would prefer a cat that is very active and intelligent. Keeping a cat like this occupied and busy is an enjoyable part of your day. Variety keeps things interesting. You don't mind a cat that is a bit fearful in new situations or around new people. You can be patient and take the time to teach him that there is nothing to fear. You prefer an affectionate cat that will snuggle with you, but you don't want a cat that is always in your face trying to get attention. A cat that is intuitive about your feelings would be ideal."
   }
 
 ];
