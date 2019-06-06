a18976aa01b9f746852bb73dcdf7a6dcc83fe271
Markus Maasikas <markus@hotmail.com>
2019-06-06 19:40:04 -0400

    commmited out match.js

--- models/match.js
+++ models/match.js
@@ -1,14 +1,23 @@
-module.exports = function (sequelize, DataTypes) {
-  var Match = sequelize.define("Example", {
-    pet_match: {
-      text: DataTypes.STRING,
-      allowNull: false
-
-    },
-    pet_rating: {
-      text: DataTypes.STRING,
-      description: DataTypes.TEXT
-    }
+module.exports = function(sequelize, DataTypes) {
+  var Example = sequelize.define("Example", {
+    text: DataTypes.STRING,
+    description: DataTypes.TEXT
   });
-  return Match;
+  return Example;
 };
+
+
+// module.exports = function (sequelize, DataTypes) {
+//   var Match = sequelize.define("Example", {
+//     pet_match: {
+//       text: DataTypes.STRING,
+//       allowNull: false
+
+//     },
+//     pet_rating: {
+//       text: DataTypes.STRING,
+//       description: DataTypes.TEXT
+//     }
+//   });
+//   return Match;
+// };
--- models/schema.sql
+++ models/schema.sql
@@ -1,14 +1,19 @@
-DROP DATABASE IF EXISTS petHappydb;
-CREATE DATABASE petHappydb;
-USE petHappydb;
+-- DROP DATABASE IF EXISTS petHappydb;
+-- CREATE DATABASE petHappydb;
+-- USE petHappydb;
 
-CREATE TABLE petHappy (
-	ItemID MEDIUMINT AUTO_INCREMENT NOT NULL,
-	burger_name varchar(255) NOT NULL,
-    devoured boolean NOT NULL,
-	PRIMARY KEY (ItemID)
-  ItemID MEDIUMINT AUTO_INCREMENT NOT NULL,
-  pet_name varchar(255) NOT NULL,
-  devoured boolean NOT NULL,
-  PRIMARY KEY(ItemID)
-);
+-- CREATE TABLE petHappy (
+-- 	ItemID MEDIUMINT AUTO_INCREMENT NOT NULL,
+-- 	burger_name varchar(255) NOT NULL,
+--     devoured boolean NOT NULL,
+-- 	PRIMARY KEY (ItemID)
+--   ItemID MEDIUMINT AUTO_INCREMENT NOT NULL,
+--   pet_name varchar(255) NOT NULL,
+--   devoured boolean NOT NULL,
+--   PRIMARY KEY(ItemID)
+-- );
+DROP DATABASE IF EXISTS exampledb;
+CREATE DATABASE exampledb;
+
+DROP DATABASE IF EXISTS testdb;
+CREATE DATABASE testdb;
