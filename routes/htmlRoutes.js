ed028c7de264bfc3942ff47991a8be237a1f532f
Markus Maasikas <markus@hotmail.com>
2019-06-10 21:05:14 -0400

    updated questions

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,51 +3,97 @@ var config = require("../config/config.json");
 
 module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
   app.get("/", function (req, res) {
-    db.Example.findAll({}).then(function (petHappydb) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.render("index");
   });
 
-  app.get("/signin", function(req, res) {
+  app.get("/signin", function (req, res) {
     res.render("signin");
   });
 
-  app.get("/signup", function(req, res) {
+  app.get("/signup", function (req, res) {
     res.render("signup");
   });
 
-  app.get("/forgot", function(req, res) {
+  app.get("/forgot", function (req, res) {
     res.render("forgot");
   });
 
-  app.get("/survey", function(req, res) {
+  app.get("/survey", function (req, res) {
     res.render("survey");
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-  app.get("/quiz", function(req, res) {
+  app.get("/quiz", function (req, res) {
     res.render("quiz");
-  });
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function (petHappydb) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
-  //HTML route for quiz page.
 
+  });
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
-};
+
+}
+
+// module.exports = function (app) {
+//   // Load index page
+//   app.get("/", function (req, res) {
+//     db.Example.findAll({}).then(function (dbExamples) {
+//       app.get("/", function (req, res) {
+//         db.Example.findAll({}).then(function (petHappydb) {
+//           res.render("index", {
+//             msg: "Welcome!",
+//             examples: dbExamples
+//           });
+//         });
+//       });
+
+
+//       app.get("/signin", function (req, res) {
+//         res.render("signin");
+//       });
+
+//       app.get("/signup", function (req, res) {
+//         res.render("signup");
+//       });
+
+//       app.get("/forgot", function (req, res) {
+//         res.render("forgot");
+//       });
+
+//       app.get("/survey", function (req, res) {
+//         res.render("survey");
+//       });
+
+//       app.get("/quiz", function (req, res) {
+//         res.render("quiz");
+
+//       });
+//       // Load example page and pass in an example by id
+//       app.get("/example/:id", function (req, res) {
+//         db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
+
+//         });
+
+//       });
+
+//       // Load example page and pass in an example by id
+//       app.get("/example/:id", function (req, res) {
+//         db.Example.findOne({ where: { id: req.params.id } }).then(function (petHappydb) {
+//           res.render("example", {
+//             example: dbExample
+//           });
+//         });
+
+//       });
+//       //HTML route for quiz page.
+
+//       // Render 404 page for any unmatched routes
+//       app.get("*", function (req, res) {
+//         res.render("404");
+//       });
+
+//     });
+
+//   });
+
+// }
