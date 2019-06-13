9eb21218b7201d3fe00d1ffce4e545de428cb7e3
JP <parker.tech9@gmail.com>
2019-06-13 16:31:33 -0400

    working get request and match page

--- public/js/petSearch.js
+++ public/js/petSearch.js
@@ -1,4 +1,33 @@
+$(function() {
 
+  var queryURL = "https://cors-anywhere.herokuapp.com/http://api.petfinder.com/pet.getRandom?format=json&key=2f95f51b181ddd27883e91878e922466&location=07728";
+  var petId = "";
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  })
+  .then(response => {
+    petId = response.petfinder.petIds.id.$t
+    console.log(petId)
+    var queryURL2 = "https://cors-anywhere.herokuapp.com/http://api.petfinder.com/pet.get?format=json&key=2f95f51b181ddd27883e91878e922466&id=" + petId
 
-// //Construct query URL to get pet data from petfinder API.
-// var queryURLBase = "https://api.petfinder.com",
+    $.ajax({
+      url: queryURL2,
+      method: 'GET'
+    })
+    .then(response => {
+      console.log(response)
+      var pet = response.petfinder.pet;
+      $("#petName").append(pet.name.$t)
+      $("#petType").append(pet.animal.$t)
+      $("#petBreed").append(pet.breeds.breed.$t)
+      $("#petGender").append(pet.sex.$t)
+      $("#petAge").append(pet.age.$t)
+      $("#petSize").append(pet.size.$t)
+      $("#petDescription").append(pet.description.$t)
+      $("#petImg").attr("src", pet.media.photos.photo[2].$t)
+    });
+  });
+
+  
+});
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -49,6 +49,11 @@ module.exports = function(app) {
     //}
   //});
 
+  app.get("/match", function(req, res) {
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.render("match");
+  });
+
   app.get("/signin", function(req, res) {
     res.render("signin");
   });
